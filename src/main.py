1d778c8d15dc72cd6016e30e8367575ab85a842a
Niatomi <playervoker@gmail.com>
2023-04-02 20:06:28 +0400

    Code refactored

--- src/main.py
+++ src/main.py
@@ -1,48 +1,33 @@
 import uvicorn
 
 from fastapi import FastAPI
-from fastapi import Request
-from fastapi import Response
-from fastapi import status
+import exceptions    
+from fastapi.middleware.cors import CORSMiddleware
 
 from exceptions import PostNotFoundException
 
-from database import Database
-
-from schemas import Post
+from posts.router import router as posts_router
 
 app = FastAPI()
 
-db = Database()
+app.include_router(router=posts_router)
 
-@app.exception_handler(PostNotFoundException)
-def handle_post_not_found_exception(request: Request, exc: PostNotFoundException):
-    return Response(
-        content="Post not found",
-        status_code=status.HTTP_404_NOT_FOUND
-    )
 
-@app.post('/create', status_code=status.HTTP_201_CREATED)
-def creae_post(post: Post):
-    return db.create_post(post)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/posts', status_code=status.HTTP_200_OK)
-def get_posts():
-    return db.get_all()
+exceptions.include_app(app)
 
-@app.get('/posts/{id}', status_code=status.HTTP_200_OK)
-def get_posts(id: int):
-    return db.get_by_id(id)
-
-@app.patch('/posts/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_post(id: int, post: Post):
-    return db.update_by_id(id, post)
-
-@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    db.delete_by_id(id)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
+# @app.on_event('startup')
+# async def start():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host='0.0.0.0', port=8000, reload=True)
