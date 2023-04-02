8a74a7452bf60574789c3543e609b5aa6a442127
Niatomi <playervoker@gmail.com>
2023-04-02 20:06:16 +0400

    Add exception handling to it's own file

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,15 @@
+from fastapi import FastAPI, HTTPException, status
+from fastapi import HTTPException
+from fastapi import status
+from fastapi import Request
+from fastapi import Response
+
 class PostNotFoundException(Exception):
-    def __init__(self, name: str = 'Post not found'):
-        self.name = name
+    pass
+
+
+def perfect_exception_handler(request: Request, exc: HTTPException):
+    return Response(status_code=status.HTTP_404_NOT_FOUND, content="Post not found ")
+
+def include_app(app):
+	app.add_exception_handler(PostNotFoundException, perfect_exception_handler)
