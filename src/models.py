7def7b227972ea82fb95733de53f09eb9bdf884e
Niatomi <playervoker@gmail.com>
2023-04-02 21:23:20 +0400

    Add new params for user table

--- src/models.py
+++ src/models.py
@@ -3,8 +3,7 @@ from database import Base
 from sqlalchemy import (
     Column,
     String,
-    Boolean,
-    Date
+    Boolean
 )
 from sqlalchemy.types import TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
@@ -12,10 +11,14 @@ from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4 as uuid
 from datetime import datetime
 
+
 class User(Base):
     __tablename__ = "user_table"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid)
-
+    name = Column(String, nullable=False)
+    login = Column(String, unique=True, nullable=False)
+    is_email_confirmed = Column(String, nullable=False)
+    
 
 class Post(Base):
     __tablename__ = "posts_table"
