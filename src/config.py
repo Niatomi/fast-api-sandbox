faf6c4536e5d1c97aae2414c4f42a90eed3acb49
Niatomi <playervoker@gmail.com>
2023-04-04 22:49:50 +0400

    Add secret env var

--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_PASS = os.environ.get('DB_PASS')
 DB_USER = os.environ.get('DB_USER')
 DB_NAME = os.environ.get('DB_NAME')
+
+SECRET = os.environ.get('DB_SECRET')
