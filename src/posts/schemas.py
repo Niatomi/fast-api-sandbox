081314b692831452a372e4df8029dd8ca40cf029
Niatomi <playervoker@gmail.com>
2023-04-04 17:50:59 +0400

    Add more schemas for posts

--- src/posts/schemas.py
+++ src/posts/schemas.py
@@ -1,6 +1,30 @@
 from pydantic import BaseModel
+from uuid import UUID
+from datetime import datetime
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    is_published: bool
+    
+    class Config:
+        orm_mode = True
+
+class PostAll(PostBase):        
+    id: UUID
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True
+
+class PostUpdate(PostBase):
+    pass
 
 class PostCreated(BaseModel):
+    message: str = "POST_IS_CREATED"
+    
+class PostDeleted(BaseModel):
     message: str = "POST_IS_CREATED"
     
 class PostUpdated(BaseModel):
