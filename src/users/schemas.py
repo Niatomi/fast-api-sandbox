75bd0dd92629626be24ac21b54d9ae985299d314
Niatomi <playervoker@gmail.com>
2023-04-04 17:51:30 +0400

    Changed messages to CAPS and add more schemas

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -4,20 +4,42 @@ from pydantic import EmailStr
 from typing import Optional
 from uuid import UUID
 
+from datetime import datetime
 
-class User(BaseModel):
-    id: Optional[UUID]
+class UserBase(BaseModel):
     name: str
     email: EmailStr
+    password: str
+    
+    class Config:
+        orm_mode = True
+
+    
+class UserCreate(UserBase):
+    pass
+
+class UserGet(UserBase):
+    id: UUID
+    is_email_confirmed: bool
+    
+    class Config:
+        orm_mode = True
+
+    
+class UserUpdate(UserBase):
+    is_email_confirmed: bool
     
 class UserCreated(BaseModel):
-    message: str = "User is created"
+    message: str = "USER_IS_CREATED"
     
 class UserDeleted(BaseModel):
-    message: str = "User deleted"
+    message: str = "USER_IS_DELETED"
     
 class UserUpdated(BaseModel):
-    message: str = "User is updated"
+    message: str = "USER_IS_UPDATED"
+
+class UserAlreadyExists(BaseModel):
+    message: str = "USER_IS_ALREADY_EXISTS"
 
 class UserNotFound(BaseModel):
-    message: str = "User is updated"
+    message: str = "USER_IS_NOT_FOUND"
