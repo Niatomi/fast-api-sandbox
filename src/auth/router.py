13ab12b0281e5bf5603333f4ad8fb54b94843ff8
Niatomi <playervoker@gmail.com>
2023-04-05 01:29:52 +0400

    Implemented user verification

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import status
+
 from database import get_async_session
 from auth.schemas import *
 from sqlalchemy.ext.asyncio import AsyncSession
 from crud.users import UserCrud
+
 from utils import *
 from exceptions import WrongCredentialsException
+from auth.oauth2 import (
+    create_access_token
+)
+
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=["Authentication"]
@@ -23,12 +30,15 @@ router = APIRouter(
                      "description": "Credentials is valid successfully logged in"
                  }
              })
-async def login(user_credentials: UserCredentials, session: AsyncSession = Depends(get_async_session)):
-    user_from_db = await UserCrud.get_by_email(session=session, email=user_credentials.email)
+async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_async_session)):
+    user_from_db = await UserCrud.get_by_email(session=session, email=user_credentials.username)
+    print(user_from_db.password)
     if not verify(user_credentials.password, user_from_db.password):
         raise WrongCredentialsException
     
     user_token = UserToken()
-    user_token.token = "123123123"
+    user_token.token = create_access_token({
+        "user_id": str(user_from_db.id), 
+        "token_type": "bearer"})
     return user_token
         
