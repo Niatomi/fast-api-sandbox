3a65641696a6e5d7601fe4e59ff58a15bb9f9337
Niatomi <playervoker@gmail.com>
2023-04-05 23:11:40 +0400

    Add dependency for checking user own of post

---
+++ src/posts/utils.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from database import get_async_session
+from crud.posts import PostsCrud
+from models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from exceptions import NotAllowedException
+from uuid import UUID
+from auth.oauth2 import get_current_user
+
+async def check_user_own(id: UUID, session: AsyncSession = Depends(get_async_session), user: User = Depends(get_current_user)):
+    post = await PostsCrud.get_by_id(session=session, id=id)
+    if post.owner_id != user.id:
+        raise NotAllowedException
