0c4024a0270e4e1b5d19a32950bc5bfda7be7444
Niatomi <playervoker@gmail.com>
2023-04-08 00:03:38 +0400

    Fixed check user own

--- src/posts/utils.py
+++ src/posts/utils.py
@@ -6,8 +6,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from posts.exceptions import NotAllowedException
 from uuid import UUID
 from auth.oauth2 import get_current_user
+from posts import schemas as posts_schemas
+from users import schemas as users_schemas
 
 async def check_user_own(id: UUID, session: AsyncSession = Depends(get_async_session), user: User = Depends(get_current_user)):
     post = await PostsCrud.get_by_id(session=session, id=id)
-    if post.owner_id != user.id:
+    if post['User'].id != user.id:
         raise NotAllowedException
